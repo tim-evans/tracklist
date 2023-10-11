e5890237126d830fe5ccd87d97b7af9dffa7338c
Glitch (glitch-hello-react) <none>
2023-10-11 04:18:51 +0000

    🚐🗽 Checkpoint ./src/app.jsx:837707/6236

--- src/app.jsx
+++ src/app.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useMemo } from "react";
 
 // Import and apply CSS stylesheet
 import "./styles/styles.css";
@@ -10,27 +10,47 @@ import Seo from "./components/seo.jsx";
 export default function Home() {
   let [cue, setCue] = useState(null);
   let [isActive, setActive] = useState(false);
-  
+
   let trackListing = useMemo(() => {
     if (cue) {
       let tracks = [];
       let lines = cue.split("\n");
       let track = {};
       for (let line of lines) {
-        let indent = line.indexOf(/^\t/);
-        if (indent === 1 && line.trim().startsWith("TRACK")) {
-          if (Object.keys(track) > 0) {
-            tracks.push(track)
+        let indent = 0;
+        for (let i = 0, len = line.length; i < len; i++) {
+          if (line[i] === "\t") {
+            indent++;
+          } else {
+            break;
+          }
+        }
+
+        line = line.trim();
+        let [key, value] = [
+          line.slice(0, line.indexOf(" ")),
+          line.slice(line.indexOf(" ") + 1),
+        ];
+        key = key.toLowerCase();
+        if (value.startsWith('"')) {
+          value = value.slice(1, -1);
+        }
+
+        if (indent === 1 && key === "track") {
+          if (Object.keys(track).length > 0) {
+            tracks.push(track);
           }
           track = {};
         } else if (indent > 1) {
-          
+          track[key] = value;
         }
       }
-      if (Object.keys(track) > 0) {
-        tracks.push(track)
+      if (Object.keys(track).length > 0) {
+        tracks.push(track);
       }
+      return tracks;
     }
+    return [];
   }, [cue]);
 
   return (
@@ -57,7 +77,13 @@ export default function Home() {
         }}
       >
         {cue == null && <>Drop a .cue file to generate a track list.</>}
-        {cue && <textarea value={cue}/>}
+        {cue && (
+          <textarea
+            value={trackListing
+              .map((track) => track.performer != null ? `${track.performer} — ${track.title}` : `${track.title}`)
+              .join("\n")}
+          />
+        )}
       </main>
     </>
   );
